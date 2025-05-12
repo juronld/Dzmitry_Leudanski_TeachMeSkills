670f5cf9e2f75d852b92d851c021834c683ddb1f
DmitryLevdansky <juronld@gmail.com>
2025-05-12 15:07:11 +0300

    Delete init.sql

--- init.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER user_dwh_load WITH PASSWORD 'user_dwh_load';
-GRANT CONNECT ON DATABASE loans TO user_dwh_load;
-GRANT USAGE ON SCHEMA public TO user_dwh_load;
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO user_dwh_load;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO user_dwh_load;
